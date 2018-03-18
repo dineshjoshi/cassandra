a4eafd10de3973b091805c2e173dedf36a34de2d
Dinesh A. Joshi <dinesh.joshi@apple.com>
2018-03-17 18:08:30 -0700

    Expose cipher suite

--- src/java/org/apache/cassandra/tools/nodetool/ClientStats.java
+++ src/java/org/apache/cassandra/tools/nodetool/ClientStats.java
@@ -21,6 +21,8 @@ import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 
+import javax.ws.rs.HEAD;
+
 import org.apache.cassandra.tools.NodeProbe;
 import org.apache.cassandra.tools.NodeTool.NodeToolCmd;
 import org.apache.cassandra.tools.nodetool.formatter.TableBuilder;
@@ -43,10 +45,10 @@ public class ClientStats extends NodeToolCmd
             if (!clients.isEmpty())
             {
                 TableBuilder table = new TableBuilder();
-                table.add("Address", "SSL", "Version", "User", "Keyspace", "Requests", "Driver-Name", "Driver-Version");
+                table.add("Address", "SSL", "Cipher", "Protocol", "Version", "User", "Keyspace", "Requests", "Driver-Name", "Driver-Version");
                 for (Map<String, String> conn : clients)
                 {
-                    table.add(conn.get("address"), conn.get("ssl"), conn.get("version"), 
+                    table.add(conn.get("address"), conn.get("ssl"), conn.get("cipher"), conn.get("protocol"), conn.get("version"),
                               conn.get("user"), conn.get("keyspace"), conn.get("requests"), conn.get("driverName"), conn.get("driverVersion"));
                 }
                 table.printTo(System.out);
